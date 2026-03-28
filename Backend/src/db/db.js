3935c8af10cd9c576004d9996887fc0f39485ddd
Shreyaaror <aroshreya222@gmail.com>
2026-03-28 11:15:27 +0530

    Removed MongoDB URI

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
  async function connectDB() {
-    await mongoose.connect("mongodb+srv://backend:shreya123@backent-prac.euun0ow.mongodb.net/project-1")
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("connected to database")
 }
 module.exports = connectDB;
